a843b0a58e4ff1ec14af81d7f8aedaad716c7f1f
swar-101 <kunwarswarprakash@gmail.com>
2025-11-18 15:10:45 +0530

    feat: Add Profile Controller

---
+++ app/microservices/user-service/src/main/java/com/nxt/user_service/controller/ProfileController.java
@@ -0,0 +1,47 @@
+package com.nxt.user_service.controller;
+
+import com.nxt.user_service.dto.UpdateProfileDTO;
+import com.nxt.user_service.entity.User;
+import com.nxt.user_service.security.CustomUserPrincipal;
+import com.nxt.user_service.service.ProfileService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/profile")
+public class ProfileController {
+
+    private final ProfileService service;
+
+    @Autowired
+    public ProfileController(ProfileService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> me(@AuthenticationPrincipal CustomUserPrincipal principal) {
+        Long userId = principal.getId();
+
+        User u = service.getById(userId);
+        u.setPasswordHash(null);
+        return ResponseEntity.ok(u);
+    }
+
+
+    @PutMapping("/me")
+    public ResponseEntity<User> update(
+            @AuthenticationPrincipal CustomUserPrincipal principal,
+            @Valid @RequestBody UpdateProfileDTO dto) {
+
+        Long userId = principal.getId(); // FIX
+
+        User updated = service.update(userId, dto);
+        updated.setPasswordHash(null);
+
+        return ResponseEntity.ok(updated);
+    }
+
+}
