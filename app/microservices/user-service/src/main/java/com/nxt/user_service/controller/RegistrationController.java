7289d7b8bebbc3c09df7dc6c1babffb91a089b1c
swar-101 <kunwarswarprakash@gmail.com>
2026-01-23 10:47:52 +0530

    test(auth): add password reset tests and test security config

--- app/microservices/user-service/src/main/java/com/nxt/user_service/controller/RegistrationController.java
+++ app/microservices/user-service/src/main/java/com/nxt/user_service/controller/RegistrationController.java
@@ -1,14 +1,7 @@
 package com.nxt.user_service.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/user")
 public class RegistrationController {
-    // new users to create an account using their email or social media profiles
-
-    @PostMapping
-    public ResponseEntity<CreateUserRespDTO> createUser(CreateUserReqDTO request) {}
-
 }
