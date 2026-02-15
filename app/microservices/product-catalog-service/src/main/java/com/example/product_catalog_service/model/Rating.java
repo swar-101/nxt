4d80bf0f0f6e90d7e163c61c8d16f84fddc4fe42
swar-101 <kunwarswarprakash@gmail.com>
2026-02-15 23:07:50 +0530

    refactor(product-catalog): remove legacy code and clean up main and test structure

--- app/microservices/product-catalog-service/src/main/java/com/example/product_catalog_service/model/Rating.java
+++
@@ -1,9 +0,0 @@
-package com.example.product_catalog_service.model;
-
-import lombok.Data;
-
-@Data
-public class Rating {
-    private Float rate;
-    private Integer count;
-}
--- app/microservices/product-catalog-service/src/main/java/com/example/product_catalog_service/repo/ProductRepository.java
+++ app/microservices/product-catalog-service/src/main/java/com/example/product_catalog_service/repo/ProductRepository.java
@@ -8,18 +8,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, PagingAndSortingRepository<Product, Long> {
-
-    Optional<Product> findById(Long id);
-
-    @Query("SELECT p FROM Product p WHERE p.name= :name")
-    List<Product> findByName(@Param("name") String name);
-
-//    List<Product> findAllByIsPrime(Boolean isPrime);
 
     @Query("""
         SELECT p FROM Product p
